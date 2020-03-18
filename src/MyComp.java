1a6ad5a9c6b45c16f230710c14ddd2705d3c6d21
Alexey Xokken <ani.xokken@gmail.com>
2020-03-18 20:48:29 +0300

    Add project

---
+++ src/MyComp.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class MyComp<T> implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1.compareTo(o2);
+    }
+}
